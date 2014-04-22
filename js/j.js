20de8ddb640e8be9402cd7eb3978f37921275182
Alexander Chernikov <alerkesi@gmail.com>
2014-04-22 13:50:24 +0400

    мелочи

--- js/j.js
+++ js/j.js
@@ -1,127 +1,127 @@
 $(document).ready(function () {
 
-  $('.search-param')
-    .on('click', '.param-name', function () {
-      $(this).parent().toggleClass('collapsed');
-      $(this).parent().toggleClass('expanded');
-    });
-  var $priceLine = $('.price-line');
-  var $showModels = $('.js-show-models');
-  var $priceFrom = $('.price-from');
-  var $priceTo = $('.price-to');
-  var $priceLineFill = $('.price-line__fill');
-  var $pFI = $('.from-input');
-  var $pTI = $('.to-input');
-  var $dragging = null;
-  var nV = 0;
-  var rightLimit = 0;
-  var leftLimit = 0;
-
-  $pFI.on('keydown', function (e) {
-    numericValidate(e);
-  })
-    .on('keyup', function () {
-      nV = parseInt($(this).val());
-      var rV = parseInt($pTI.val());
-      if (nV < rV && nV >= 0) {
-        moveFrom($priceFrom, nV);
-      }
-
-    });
-  $pTI.on('keyup', function () {
-    nV = parseInt($(this).val());
-    var lV = parseInt($pFI.val());
-    if (nV > lV) {
-      moveTo($priceTo, Math.min(nV, 24000));
-    }
-  })
-    .on('keydown', function (e) {
-      numericValidate(e);
-    });
-  $priceLine.on("mousedown", ".price-toggle", function (e) {
-    e.preventDefault();
-    $dragging = $(e.target);
-  });
-  $(document).on("mousemove", function (e) {
-    if ($dragging) {
-      nV = e.pageX - 5 - $priceLine.offset().left;
-      rightLimit = parseInt($priceTo.css('left'));
-      leftLimit = parseInt($priceFrom.css('left'));
-      if (($dragging.hasClass('price-from')) && (nV >= 0) && (nV < rightLimit)) {
-        $dragging.css('left', nV);
-        $pFI.val(Math.round(nV / ($priceLine.width() - $dragging.width() - 1) * 24000));
-        $priceLineFill.css({
-          left: nV + 5,
-          width: rightLimit - nV
+    $('.search-param')
+        .on('click', '.param-name', function () {
+            $(this).parent().toggleClass('collapsed');
+            $(this).parent().toggleClass('expanded');
         });
-      }
-      if (($dragging.hasClass('price-to')) && (nV <= ($priceLine.width() - $dragging.width() - 1)) && (nV > leftLimit )) {
-        $dragging.css('left', nV);
-        $pTI.val(Math.round(nV / ($priceLine.width() - $dragging.width() - 1) * 24000));
-        $priceLineFill.css({
-          width: nV - leftLimit + 1
+    var $priceLine = $('.price-line');
+    var $showModels = $('.js-show-models');
+    var $priceFrom = $('.price-from');
+    var $priceTo = $('.price-to');
+    var $priceLineFill = $('.price-line__fill');
+    var $pFI = $('.from-input');
+    var $pTI = $('.to-input');
+    var $dragging = null;
+    var nV = 0;
+    var rightLimit = 0;
+    var leftLimit = 0;
+
+    $pFI.on('keydown', function (e) {
+        numericValidate(e);
+    })
+        .on('keyup', function () {
+            nV = parseInt($(this).val());
+            var rV = parseInt($pTI.val());
+            if (nV < rV && nV >= 0) {
+                moveFrom($priceFrom, nV);
+            }
+
         });
-      }
-      $showModels.show(0).delay(5000).hide(0);
-    }
-
-  });
-  var moveFrom = function (elem, nV) {
-    var newPos = nV / 24000 * ($priceLine.width() - elem.width() - 1);
-    rightLimit = parseInt($priceTo.css('left'));
-    elem.css('left', newPos);
-    $priceLineFill.css({
-      left: newPos + 5,
-      width: rightLimit - newPos
+    $pTI.on('keyup', function () {
+        nV = parseInt($(this).val());
+        var lV = parseInt($pFI.val());
+        if (nV > lV) {
+            moveTo($priceTo, Math.min(nV, 24000));
+        }
+    })
+        .on('keydown', function (e) {
+            numericValidate(e);
+        });
+    $priceLine.on("mousedown", ".price-toggle", function (e) {
+        e.preventDefault();
+        $dragging = $(e.target);
     });
-    $showModels.show(0).delay(5000).hide(0);
-  };
-  var moveTo = function (elem, nV) {
-    var newPos = nV / 24000 * ($priceLine.width() - elem.width() - 1);
-    leftLimit = parseInt($priceFrom.css('left'));
-    elem.css('left', newPos);
-    $priceLineFill.css({
-      width: newPos - leftLimit + 1
-    });
-    $showModels.show(0).delay(5000).hide(0);
-  };
-  var numericValidate = function (e) {
-    if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 110, 190]) !== -1 ||
-      // Allow: Ctrl+A
-      (e.keyCode == 65 && e.ctrlKey === true) ||
-      (e.keyCode == 67 && e.ctrlKey === true) ||
-      (e.keyCode == 86 && e.ctrlKey === true) ||
-      // Allow: home, end, left, right
-      (e.keyCode >= 35 && e.keyCode <= 39)) {
-      return;
-    }
-    if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
-      e.preventDefault();
-    }
-  };
-  $(document).on("mouseup", function (e) {
-    $dragging = null;
+    $(document).on("mouseup", function (e) {
+        $dragging = null;
 
-  });
-  var tV = $('.js-toggleView');
-  tV.click(function (e) {
-    e.preventDefault();
-    tV.removeClass('selected');
-    $(this).addClass('selected');
-    $('.dash-catalog').toggle();
-    $('.list-catalog').toggle();
-  });
-  var tS = $('.js-sort');
-  tS.click(function (e) {
-    e.preventDefault();
-    var $this = $(this);
-    if ($this.hasClass('asc')) {
-      $this.removeClass('asc');
-      $this.addClass('desc');
-    } else {
-      tS.removeClass('desc');
-      tS.removeClass('asc');
-      $this.addClass('asc');
-    }
-  })
+    });
+    $(document).on("mousemove", function (e) {
+        if ($dragging) {
+            nV = e.pageX - 5 - $priceLine.offset().left;
+            rightLimit = parseInt($priceTo.css('left'));
+            leftLimit = parseInt($priceFrom.css('left'));
+            if (($dragging.hasClass('price-from')) && (nV >= 0) && (nV < rightLimit)) {
+                $dragging.css('left', nV);
+                $pFI.val(Math.round(nV / ($priceLine.width() - $dragging.width() - 1) * 24000));
+                $priceLineFill.css({
+                    left: nV + 5,
+                    width: rightLimit - nV
+                });
+            }
+            if (($dragging.hasClass('price-to')) && (nV <= ($priceLine.width() - $dragging.width() - 1)) && (nV > leftLimit )) {
+                $dragging.css('left', nV);
+                $pTI.val(Math.round(nV / ($priceLine.width() - $dragging.width() - 1) * 24000));
+                $priceLineFill.css({
+                    width: nV - leftLimit + 1
+                });
+            }
+            $showModels.show(0).delay(5000).hide(0);
+        }
+
+    });
+    var moveFrom = function (elem, nV) {
+        var newPos = nV / 24000 * ($priceLine.width() - elem.width() - 1);
+        rightLimit = parseInt($priceTo.css('left'));
+        elem.css('left', newPos);
+        $priceLineFill.css({
+            left: newPos + 5,
+            width: rightLimit - newPos
+        });
+        $showModels.show(0).delay(5000).hide(0);
+    };
+    var moveTo = function (elem, nV) {
+        var newPos = nV / 24000 * ($priceLine.width() - elem.width() - 1);
+        leftLimit = parseInt($priceFrom.css('left'));
+        elem.css('left', newPos);
+        $priceLineFill.css({
+            width: newPos - leftLimit + 1
+        });
+        $showModels.show(0).delay(5000).hide(0);
+    };
+    var numericValidate = function (e) {
+        if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 110, 190]) !== -1 ||
+            // Allow: Ctrl+A
+            (e.keyCode == 65 && e.ctrlKey === true) ||
+            (e.keyCode == 67 && e.ctrlKey === true) ||
+            (e.keyCode == 86 && e.ctrlKey === true) ||
+            // Allow: home, end, left, right
+            (e.keyCode >= 35 && e.keyCode <= 39)) {
+            return;
+        }
+        if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
+            e.preventDefault();
+        }
+    };
+    var tV = $('.js-toggleView');
+    tV.click(function (e) {
+        e.preventDefault();
+        tV.removeClass('selected');
+        $(this).addClass('selected');
+        $('.dash-catalog').toggle();
+        $('.list-catalog').toggle();
+    });
+    var tS = $('.js-sort');
+    tS.click(function (e) {
+        e.preventDefault();
+        var $this = $(this);
+        if ($this.hasClass('asc')) {
+            $this.removeClass('asc');
+            $this.addClass('desc');
+        } else {
+            tS.removeClass('desc');
+            tS.removeClass('asc');
+            $this.addClass('asc');
+        }
+    })
 });
